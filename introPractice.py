26af94a5836b7d2ac7dc1cc4b5d8652557842d39
ankitgujre <kumarankit65494@gmail.com>
2025-01-09 13:49:45 +0530

    first-commit

---
+++ introPractice.py
@@ -0,0 +1,20 @@
+#  We can define the function using the def keyword. followed by the function name and parentheses () and colon :.
+def my_function():
+  print("Hello from a function")
+  
+  # To call a function, use the function name followed by parentheses:
+my_function()
+print("second time calling the function")
+my_function()
+
+# Arguments
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+def multiply(a,b):
+#   return a*b
+    print(a*b)
+
+multiply(3,4)
+
+multiply(5,6)
+multiply(7,8)
